dce370b681f1710adfc30906f77e2580f3cdfb8f
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-16 21:18:58 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,9 @@
 def using_while
   levitation_force = 6
 	while levitation_force < 10
- num_of_hotdogs_eaten += 1
- puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+ levitation_force += 1
+ puts "Wingardium Leviosa"
 end
-
-  #your code here
 end
 
 
